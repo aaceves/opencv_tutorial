2d0407a07744a90f4a270d41a738c040fdd106f6
aaceves <aaceves@users.noreply.github.com>
2019-06-05 16:37:47 -0500

    Se corrigió path absoluto

--- src/toGray.cpp
+++ src/toGray.cpp
@@ -8,19 +8,22 @@ int main(int argc, char **argv){
     // -------------------------- INICIA CODIGO DE OPENCV --------------------------
     cv::Mat image;
     cv::Mat gray_image;
-    image = cv::imread("red_panda.jpg"); 
+    image = cv::imread("/home/kinetic/catkin_ws/src/opencv_tutorial/img/red_panda.jpg"); 
     if (!image.data) {
         std::cout << "Error al cargar imagen: red_panda.jpg"<< std::endl;
         exit(1);
     }
+    cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
     cv::imshow("Red panda", image);
     cv::imshow("Gray panda", gray_image);
+    std::cout << "Hit ENTER to close window images." << std::endl;
     cv::waitKey(0);
     cv::destroyAllWindows();
-    cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
+    
     //std::cout << "Dimensión de la variable image: " << image.shape << std::endl;
     //std::cout << "Dimensión de la variable gray_image: " << gray_image.shape << std::endl;
-    cv::imwrite("gray_panda.jpg", gray_image);
+
+    cv::imwrite("/home/kinetic/catkin_ws/src/opencv_tutorial/img/gray_panda2.jpg", gray_image);
     // -------------------------- TERMINA CODIGO DE OPENCV --------------------------
     ros::spinOnce();
     return 0;
