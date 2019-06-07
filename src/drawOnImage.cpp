059b1612351c314c581e09e9697ddc0eab2fee2b
aaceves <aaceves@users.noreply.github.com>
2019-06-07 12:11:35 -0500

    Update drawOnImage.cpp

--- src/drawOnImage.cpp
+++ src/drawOnImage.cpp
@@ -1,44 +1,57 @@
-//https://docs.opencv.org/3.4/dc/da5/tutorial_py_drawing_functions.html
-//https://pysource.com/2018/01/22/drawing-and-writing-on-images-opencv-3-4-with-python-3-tutorial-3/
-//https://docs.opencv.org/3.1.0/d3/d96/tutorial_basic_geometric_drawing.html
-//http://opencvexamples.blogspot.com/2013/10/basic-drawing-examples.html
-//https://docs.opencv.org/3.4/d3/d96/tutorial_basic_geometric_drawing.html
-//http://opencvexamples.blogspot.com/p/learning-opencv-functions-step-by-step.html
-
-
 #include "ros/ros.h"
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv){
-    ros::init(argc, argv, "showimage");
+    ros::init(argc, argv, "showimage3");
     ros::NodeHandle n;
     ros::Rate loop_rate(0.5);
     // -------------------------- INICIA CODIGO DE OPENCV --------------------------
-    // Based on: https://docs.opencv.org/3.2.0/db/d64/tutorial_load_save_image.html
-    cv::Mat image;
-    cv::Mat gray_image;
+    // Based on: https://docs.opencv.org/3.1.0/d3/d96/tutorial_basic_geometric_drawing.html
 
-    image = cv::imread("/home/kinetic/catkin_ws/src/opencv_tutorial/img/red_panda.jpg"); 
-    if (image.empty()) { // or (!image.data)
-        std::cout << "Error al cargar imagen."<< std::endl;
-        exit(1);
-    }
-    std::cout << "Height : " << image.rows << std::endl;  // or image.size().height
-    std::cout << "Width : " << image.cols << std::endl;  // or image.size().width
+    cv::Mat image = cv::Mat::zeros(512, 512, CV_8UC3);
+    
+    cv::Scalar redColor     =  cv::Scalar(  0,   0, 255);
+    cv::Scalar greenColor   =  cv::Scalar(  0, 255,   0);
+    cv::Scalar blueColor    =  cv::Scalar(255,   0,   0);
+    cv::Scalar magentaColor =  cv::Scalar(255,   0, 255);
+    cv::Scalar whiteColor   =  cv::Scalar(255, 255, 255);
 
-    cv::namedWindow("Red panda");         // Crea una ventana con un nombre específico
-    cv::moveWindow("Red panda", 100,100); // Mueve la ventana a la posición x,y
-    cv::namedWindow("Gray panda");
-    cv::moveWindow("Gray panda", 200,100);
+    cv::Point startingPoint, endingPoint, centerPoint;
+    int thickness = 2, lineType = cv::LINE_8;
 
-    cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY); // Convierte de BGR a GRAY
-    cv::imshow("Red panda", image);
-    cv::imshow("Gray panda", gray_image);
-    std::cout << "Hit ENTER to close window images." << std::endl;
+
+    startingPoint =  cv::Point(0,0); 
+    endingPoint =  cv::Point(511,511);
+    cv::line( image, startingPoint, endingPoint, redColor, thickness, lineType);
+
+
+    startingPoint =  cv::Point(384,0);
+    endingPoint =  cv::Point(510,128);
+    cv::rectangle( image,  startingPoint,      endingPoint, greenColor, thickness, lineType);
+    cv::rectangle( image, cv::Point(1,1), cv::Point(30,60), greenColor, -1); // Solid rectangle
+
+
+    int radius = 63;
+    centerPoint = cv::Point(447,63);
+    cv::circle( image, centerPoint, radius, magentaColor, 1, lineType);
+
+
+    centerPoint = cv::Point(256,256);
+    cv::Size principalAxis = cv::Size(100,50);
+    double rotationAngle = 45, startingAngle = 0, endingAngle = 360; // Angles in degress
+    cv::ellipse( image, centerPoint, principalAxis, rotationAngle, startingAngle, endingAngle, blueColor, 8, lineType);
+    cv::ellipse( image, centerPoint, principalAxis, 0, 0, 180, redColor, -1); // Filled sector
+
+
+    double fontScale = 4;
+    bool bottomLeftOrigin = false;
+    int font = cv::FONT_HERSHEY_SIMPLEX;
+    cv::putText(image,"OpenCV", cv::Point(20,500), font, fontScale, whiteColor, thickness, lineType, bottomLeftOrigin);
+
+
+    cv::imshow("Image",image);
     cv::waitKey(0);
     cv::destroyAllWindows();
-
-    cv::imwrite("/home/kinetic/catkin_ws/src/opencv_tutorial/img/gray_panda.jpg", gray_image);
     // -------------------------- TERMINA CODIGO DE OPENCV --------------------------
     ros::spinOnce();
     return 0;
