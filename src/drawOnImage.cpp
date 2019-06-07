217cd70d107afb111f3194632e8e3408611dbcb5
aaceves <aaceves@users.noreply.github.com>
2019-06-07 10:11:02 -0500

    Create drawOnImage.cpp

---
+++ src/drawOnImage.cpp
@@ -0,0 +1,6 @@
+https://docs.opencv.org/3.4/dc/da5/tutorial_py_drawing_functions.html
+https://pysource.com/2018/01/22/drawing-and-writing-on-images-opencv-3-4-with-python-3-tutorial-3/
+https://docs.opencv.org/3.1.0/d3/d96/tutorial_basic_geometric_drawing.html
+http://opencvexamples.blogspot.com/2013/10/basic-drawing-examples.html
+https://docs.opencv.org/3.4/d3/d96/tutorial_basic_geometric_drawing.html
+http://opencvexamples.blogspot.com/p/learning-opencv-functions-step-by-step.html
