cd3bd8a0a1c3db6a54f3f478c968f10d711d0e18
aaceves <aaceves@users.noreply.github.com>
2019-06-12 12:59:23 -0500

    Update readWriteFile.cpp

--- src/readWriteFile.cpp
+++ src/readWriteFile.cpp
@@ -1,8 +1,51 @@
-// 
+#include "ros/ros.h"
+#include <opencv2/opencv.hpp>
 
-// Read this:
-// https://stackoverflow.com/questions/26948882/run-program-save-data-in-text-file-and-read-it-back
-// Watch this:
-// https://www.youtube.com/watch?v=13TrhiKLZg8
+int main(int argc, char **argv){
+    ros::init(argc, argv, "read_write_file");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(0.5);
+    // -------------------------- INICIA CODIGO --------------------------
+    // Based on: https://www.youtube.com/watch?v=lzxWNtjii8U
 
-//
+
+    // To write in a file
+    std::ofstream ofile_handler;
+    ofile_handler.open("colorSpace_thresholds.txt");
+    if (ofile_handler.fail()) {
+        std::cerr << "Error on creating file." << std::endl;
+        exit(1);
+    }
+    ofile_handler << "low_H  " << 123 << std::endl; // This is the first line
+    ofile_handler << "high_H " << 222 << std::endl; 
+    ofile_handler << "low_S  " << 321 << std::endl; 
+    ofile_handler << "high_S " << 404 << std::endl;
+    ofile_handler << "low_I  " << 555 << std::endl;
+    ofile_handler << "high_I " << 654 << std::endl; // This is the last line
+    ofile_handler.close();
+    std::cout << "File saved. Hit ENTER to continue ... " << std::endl;
+    std::cin.get();
+
+
+    // To read from a file
+    std::ifstream ifile_handler;
+    std::string name;
+    int value;
+    ifile_handler.open("colorSpace_thresholds.txt");
+    if (ifile_handler.is_open()) {
+        while(ifile_handler >> name >> value) {
+            std::cout << name << " " << value << std::endl;
+        }
+        ifile_handler.close();
+    }
+    else {
+        std::cout << "File not found" << std::endl;
+        exit(2);
+    }
+    
+    std::cout << "\nHit ENTER to finish." << std::endl;
+    std::cin.get();
+    // -------------------------- TERMINA CODIGO --------------------------
+    ros::spinOnce();
+    return 0;
+}
