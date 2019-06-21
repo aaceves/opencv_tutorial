fbcd289d99d56016689bed8a33cb33c9b0f3215b
aaceves <aaceves@users.noreply.github.com>
2019-06-21 18:41:40 -0500

    Add files via upload

---
+++ src/findContours.cpp
@@ -0,0 +1,5 @@
+// http://opencvexamples.blogspot.com/2013/09/find-contour.html
+// https://stackoverflow.com/questions/8449378/finding-contours-in-opencv
+// https://stackoverflow.com/questions/46187563/finding-largest-contours-c
+// https://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/find_contours/find_contours.html
+// https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
